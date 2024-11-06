3222d0ade2bd3d2ea6a5044db99079dbcd8bd060
TrongVan123 <trongchu147@gmail.com>
2024-11-06 09:42:03 +0700

    update QuanLyTL

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import MainScreen from './src/screens/MainScreen/trangchu';
 import LoginScreen from './src/screens/LoginScreen/login';
 import RegisterScreen from './src/screens/RegisterScreen/register';
-import {UserProvider} from './src/context/UserContext.js';
+import { UserProvider } from './src/context/UserContext.js';
 import PublisherManagementScreen from './src/screens/PublisherManagementScreen/publisher.js';
 import CartScreen from './src/screens/CartScreen/index.js';
+import AdminCatagoryScreen from './src/screens/TheloaiScreen/theloai';
 const Stack = createStackNavigator();
 
 export default function App() {
@@ -14,13 +15,14 @@ export default function App() {
     <UserProvider>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="MainScreen" screenOptions={{
-                    headerShown:false,
-                }}>
+          headerShown: false,
+        }}>
           <Stack.Screen name="MainScreen" component={MainScreen} />
           <Stack.Screen name="LoginScreen" component={LoginScreen} />
           <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
           <Stack.Screen name="CartScreen" component={CartScreen} />
           <Stack.Screen name="PublisherManagementScreen" component={PublisherManagementScreen} />
+          <Stack.Screen name="AdminCatagoryScreen" component={AdminCatagoryScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </UserProvider>
