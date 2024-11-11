c740243e58462572c4d2cb95cb161c2d6d8440fb
vtphuc0271 <emtai789@gmail.com>
2024-11-11 09:02:40 +0700

    fix error #2

--- src/screens/BookManagementScreen/BookManagement.js
+++ src/screens/BookManagementScreen/BookManagement.js
@@ -5,6 +5,7 @@ import CheckBox from '@react-native-community/checkbox';
 import { launchImageLibrary } from 'react-native-image-picker';
 import firestore from '@react-native-firebase/firestore';
 import storage from '@react-native-firebase/storage';
+import NavbarCard from '../../components/NavbarCard';
 
 const BookManagement = () => {
   const [Sach, setBooks] = useState([]);
@@ -292,175 +293,181 @@ const BookManagement = () => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.header1}>
-        <Text style={styles.headerText}>Quản lý sách</Text>
-      </View>
-      <View style={styles.header}>
-        <View style={styles.actionButtons}>
-          <TouchableOpacity style={styles.addButton} onPress={() => setModalVisible(true)}>
-            <Text style={styles.buttonText}>+ Thêm mới</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.deleteAllButton} onPress={() => setBooks([])}>
-            <Text style={styles.buttonText}>Xóa tất cả</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.searchContainer}>
-          <TextInput
-            placeholder="Tìm kiếm"
-            placeholderTextColor="#000"
-            style={styles.searchInput}
-            value={searchText}
-            onChangeText={setSearchText}
-          />
-        </View>
-      </View>
-      <View style={styles.descriptionRow}>
-        <Text style={[styles.categoryId, { flex: 0.9 }]}>STT</Text>
-        <Text style={[styles.categoryName, { flex: 1.1 }]}>Ảnh</Text>
-        <Text style={[styles.categoryName, { flex: 2 }]}>Tên sách</Text>
-        <Text style={styles.categoryCheckbox}>Top</Text>
-        <Text style={styles.categoryCheckbox}>Hiển thị</Text>
-        <Text style={[styles.actionIcons, { flex: 1.5 }]}>Hành động</Text>
-      </View>
-      <ScrollView>
-        {filteredBooks.map((book, index) => (
-
-          <View key={book.id} style={styles.categoryRow}>
-            <View style={{ backgroundColor: '#D9D9D9', marginRight: 5, width: 40, height: 40, justifyContent: 'center', alignItems: 'center' }}>
-              <Text style={[styles.categoryId, { flex: 0.7 }]}>{index + 1}</Text>
-            </View>
-            <View style={[styles.imageContainer, { flex: 1 }]}>
-              {book.anhSach ? (
-                <Image source={{ uri: book.anhSach }} style={styles.categoryImage} />
-              ) : (
-                <Image source={require('../../assets/default.png')} style={styles.categoryImage} />
-              )}
-            </View>
-            <Text style={[styles.categoryName, { flex: 1.8 }]}>{book.tenSach}</Text>
-            <CheckBox value={book.isTop} onValueChange={() => toggleProperty(book.id, 'isTop')} tintColors={{ true: '#0096FF', false: '#D9D9D9' }} />
-            <CheckBox value={book.displayed} onValueChange={() => toggleProperty(book.id, 'displayed')} tintColors={{ true: '#0096FF', false: '#D9D9D9' }} />
-            <View style={[styles.actionIcons, { flex: 1.5 }]}>
-              <TouchableOpacity
-                onPress={() => {
-                  setNewBook(book);
-                  setEditBookId(book.id);
-                  setIsEditing(true);
-                  setModalVisible(true);
-                }}
-              >
-                <Image source={require('../../assets/edit.png')} />
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => handleDelete(book.id)}>
-                <Image source={require('../../assets/delete.png')} />
-              </TouchableOpacity>
-            </View>
+      <NavbarCard ScreenName={'Quản lý sách'}
+        iconShop={true}>
+      </NavbarCard>
+      <View style={styles.container2}>
+        <View style={styles.header}>
+          <View style={styles.actionButtons}>
+            <TouchableOpacity style={styles.addButton} onPress={() => setModalVisible(true)}>
+              <Text style={styles.buttonText}>+ Thêm mới</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.deleteAllButton} onPress={() => setBooks([])}>
+              <Text style={styles.buttonText}>Xóa tất cả</Text>
+            </TouchableOpacity>
           </View>
-        ))}
-      </ScrollView>
-      <Modal animationType="slide" transparent={true} visible={modalVisible} onRequestClose={() => setModalVisible(!modalVisible)}>
-        <View style={styles.modalContainer}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>{isEditing ? 'Chỉnh sửa Sách' : 'Thêm Sách'} </Text>
-            <ScrollView>
-              <TextInput
-                placeholder="Tên sách"
-                placeholderTextColor="#aaa"
-                style={styles.input}
-                value={newBook.tenSach}
-                onChangeText={text => setNewBook({ ...newBook, tenSach: text })}
-              />
-              <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                {renderPicker('Tác giả', newBook.tacGia, itemValue => setNewBook({ ...newBook, tacGia: itemValue }), tacGia)}
-                <View style={{ width: 10 }}></View>
-                <TouchableOpacity onPress={() => setAuthorModalVisible(true)}>
-                  <Text style={styles.buttonTG}>{'Thêm mới'}</Text>
+          <View style={styles.searchContainer}>
+            <TextInput
+              placeholder="Tìm kiếm"
+              placeholderTextColor="#000"
+              style={styles.searchInput}
+              value={searchText}
+              onChangeText={setSearchText}
+            />
+          </View>
+        </View>
+        <View style={styles.descriptionRow}>
+          <Text style={[styles.categoryId, { flex: 0.9 }]}>STT</Text>
+          <Text style={[styles.categoryName, { flex: 1.1 }]}>Ảnh</Text>
+          <Text style={[styles.categoryName, { flex: 2 }]}>Tên sách</Text>
+          <Text style={styles.categoryCheckbox}>Top</Text>
+          <Text style={styles.categoryCheckbox}>Hiển thị</Text>
+          <Text style={[styles.actionIcons, { flex: 1.5 }]}>Hành động</Text>
+        </View>
+        <ScrollView>
+          {filteredBooks.map((book, index) => (
+
+            <View key={book.id} style={styles.categoryRow}>
+              <View style={{ backgroundColor: '#D9D9D9', marginRight: 5, width: 40, height: 40, justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={[styles.categoryId, { flex: 0.7 }]}>{index + 1}</Text>
+              </View>
+              <View style={[styles.imageContainer, { flex: 1 }]}>
+                {book.anhSach ? (
+                  <Image source={{ uri: book.anhSach }} style={styles.categoryImage} />
+                ) : (
+                  <Image source={require('../../assets/default.png')} style={styles.categoryImage} />
+                )}
+              </View>
+              <Text style={[styles.categoryName, { flex: 1.8 }]}>{book.tenSach}</Text>
+              <CheckBox value={book.isTop} onValueChange={() => toggleProperty(book.id, 'isTop')} tintColors={{ true: '#0096FF', false: '#D9D9D9' }} />
+              <CheckBox value={book.displayed} onValueChange={() => toggleProperty(book.id, 'displayed')} tintColors={{ true: '#0096FF', false: '#D9D9D9' }} />
+              <View style={[styles.actionIcons, { flex: 1.5 }]}>
+                <TouchableOpacity
+                  onPress={() => {
+                    setNewBook(book);
+                    setEditBookId(book.id);
+                    setIsEditing(true);
+                    setModalVisible(true);
+                  }}
+                >
+                  <Image source={require('../../assets/edit.png')} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => handleDelete(book.id)}>
+                  <Image source={require('../../assets/delete.png')} />
                 </TouchableOpacity>
               </View>
-              <Modal animationType="slide" transparent={true} visible={authorModalVisible} onRequestClose={() => setAuthorModalVisible(!authorModalVisible)}>
-                <View style={styles.modalContainer}>
-                  <View style={styles.modalView}>
-                    <Text style={styles.modalText}>{'Thêm Tác Giả'}</Text>
-                    <View style={{alignItems:'center', marginBottom:10}}>
-                    <TouchableOpacity  style={{ borderWidth: 3, width: 120, height: 120, justifyContent: 'center', alignItems: 'center', borderRadius: 5, borderColor: '#969292' }} onPress={selectAuthorImage}>
-                      {newAuthorImage ? (
-                        <Image source={{ uri: newAuthorImage }} style={styles.imagePreview} />
-                      ) : (
-                        <Image source={require('../../assets/default.png')} style={styles.imagePreview} />
-                      )}
-                    </TouchableOpacity>
-                    </View>
-                   
-                    <TextInput placeholder="Tên tác giả" placeholderTextColor="#000" style={styles.input} value={newAuthorName} onChangeText={setNewAuthorName} />
-                    <TextInput placeholder="Năm sinh" placeholderTextColor="#000" style={styles.input} keyboardType="numeric" value={newAuthorBirthYear} onChangeText={setNewAuthorBirthYear} />
-                    <TouchableOpacity style={[styles.button, { backgroundColor: 'green' }]} onPress={handleAddNewAuthor}>
-                      <Text style={styles.buttonText}>{'Thêm'}</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={[styles.button, { backgroundColor: 'red' }]} onPress={() => setAuthorModalVisible(false)}>
-                      <Text style={styles.buttonText}>Đóng</Text>
-                    </TouchableOpacity>
-                  </View>
-                </View>
-              </Modal>
-
-              {renderPicker('Nhà xuất bản', newBook.nhaXuatBan, itemValue => setNewBook({ ...newBook, nhaXuatBan: itemValue }), nhaXuatBan)}
-              <TextInput
-                placeholder="Mô tả"
-                placeholderTextColor="#aaa"
-                style={[styles.input, { height: 80 }]}
-                value={newBook.moTa}
-                multiline
-                numberOfLines={4}
-                onChangeText={text => setNewBook({ ...newBook, moTa: text })}
-              />
-              <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-
-                {renderPicker('Thể loại', newBook.theLoai, itemValue => setNewBook({ ...newBook, theLoai: itemValue }), theLoai)}
-                <View style={{ width: 10 }}></View>
+            </View>
+          ))}
+        </ScrollView>
+        <Modal animationType="slide" transparent={true} visible={modalVisible} onRequestClose={() => setModalVisible(!modalVisible)}>
+          <View style={styles.modalContainer}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>{isEditing ? 'Chỉnh sửa Sách' : 'Thêm Sách'} </Text>
+              <ScrollView>
                 <TextInput
-                  placeholder="Năm xuất bản"
+                  placeholder="Tên sách"
                   placeholderTextColor="#aaa"
                   style={styles.input}
-                  value={newBook.namXuatBan}
-                  onChangeText={text => setNewBook({ ...newBook, namXuatBan: text })}
+                  value={newBook.tenSach}
+                  onChangeText={text => setNewBook({ ...newBook, tenSach: text })}
+                />
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                  {renderPicker('Tác giả', newBook.tacGia, itemValue => setNewBook({ ...newBook, tacGia: itemValue }), tacGia)}
+                  <View style={{ width: 10 }}></View>
+                  <TouchableOpacity onPress={() => setAuthorModalVisible(true)}>
+                    <Text style={styles.buttonTG}>{'Thêm mới'}</Text>
+                  </TouchableOpacity>
+                </View>
+                <Modal animationType="slide" transparent={true} visible={authorModalVisible} onRequestClose={() => setAuthorModalVisible(!authorModalVisible)}>
+                  <View style={styles.modalContainer}>
+                    <View style={styles.modalView}>
+                      <Text style={styles.modalText}>{'Thêm Tác Giả'}</Text>
+                      <View style={{ alignItems: 'center', marginBottom: 10 }}>
+                        <TouchableOpacity style={{ borderWidth: 3, width: 120, height: 120, justifyContent: 'center', alignItems: 'center', borderRadius: 5, borderColor: '#969292' }} onPress={selectAuthorImage}>
+                          {newAuthorImage ? (
+                            <Image source={{ uri: newAuthorImage }} style={styles.imagePreview} />
+                          ) : (
+                            <Image source={require('../../assets/default.png')} style={styles.imagePreview} />
+                          )}
+                        </TouchableOpacity>
+                      </View>
+
+                      <TextInput placeholder="Tên tác giả" placeholderTextColor="#000" style={styles.input} value={newAuthorName} onChangeText={setNewAuthorName} />
+                      <TextInput placeholder="Năm sinh" placeholderTextColor="#000" style={styles.input} keyboardType="numeric" value={newAuthorBirthYear} onChangeText={setNewAuthorBirthYear} />
+                      <TouchableOpacity style={[styles.button, { backgroundColor: 'green' }]} onPress={handleAddNewAuthor}>
+                        <Text style={styles.buttonText}>{'Thêm'}</Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity style={[styles.button, { backgroundColor: 'red' }]} onPress={() => setAuthorModalVisible(false)}>
+                        <Text style={styles.buttonText}>Đóng</Text>
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                </Modal>
+
+                {renderPicker('Nhà xuất bản', newBook.nhaXuatBan, itemValue => setNewBook({ ...newBook, nhaXuatBan: itemValue }), nhaXuatBan)}
+                <TextInput
+                  placeholder="Mô tả"
+                  placeholderTextColor="#aaa"
+                  style={[styles.input, { height: 80 }]}
+                  value={newBook.moTa}
+                  multiline
+                  numberOfLines={4}
+                  onChangeText={text => setNewBook({ ...newBook, moTa: text })}
+                />
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+
+                  {renderPicker('Thể loại', newBook.theLoai, itemValue => setNewBook({ ...newBook, theLoai: itemValue }), theLoai)}
+                  <View style={{ width: 10 }}></View>
+                  <TextInput
+                    placeholder="Năm xuất bản"
+                    placeholderTextColor="#aaa"
+                    style={styles.input}
+                    value={newBook.namXuatBan}
+                    onChangeText={text => setNewBook({ ...newBook, namXuatBan: text })}
+                    keyboardType="numeric"
+                  />
+                </View>
+                <TextInput
+                  placeholder="Giá tiền"
+                  placeholderTextColor="#aaa"
+                  style={styles.input}
+                  value={newBook.giaTien}
+                  onChangeText={text => setNewBook({ ...newBook, giaTien: text })}
                   keyboardType="numeric"
                 />
-              </View>
-              <TextInput
-                placeholder="Giá tiền"
-                placeholderTextColor="#aaa"
-                style={styles.input}
-                value={newBook.giaTien}
-                onChangeText={text => setNewBook({ ...newBook, giaTien: text })}
-                keyboardType="numeric"
-              />
-              <View style={{ alignItems: 'center' }}>
-                <TouchableOpacity onPress={selectImage} style={{ borderWidth: 3, width: 120, height: 120, justifyContent: 'center', alignItems: 'center', borderRadius: 5, borderColor: '#969292' }}>
-                  {newBook.anhSach ? (
-                    <Image source={{ uri: newBook.anhSach }} style={{ width: 100, height: 100, margin: 10 }} />
-                  ) : (
-                    <Image source={require('../../assets/default.png')} style={{ width: 100, height: 100, margin: 10 }} />
-                  )}
+                <View style={{ alignItems: 'center' }}>
+                  <TouchableOpacity onPress={selectImage} style={{ borderWidth: 3, width: 120, height: 120, justifyContent: 'center', alignItems: 'center', borderRadius: 5, borderColor: '#969292' }}>
+                    {newBook.anhSach ? (
+                      <Image source={{ uri: newBook.anhSach }} style={{ width: 100, height: 100, margin: 10 }} />
+                    ) : (
+                      <Image source={require('../../assets/default.png')} style={{ width: 100, height: 100, margin: 10 }} />
+                    )}
+                  </TouchableOpacity>
+                </View>
+              </ScrollView>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                <TouchableOpacity style={[styles.button, { backgroundColor: 'green' }]} onPress={isEditing ? handleEditBook : handleAddNew}>
+                  <Text style={styles.buttonText}>{isEditing ? 'Sửa' : 'Thêm'}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.button, { backgroundColor: 'red' }]} onPress={resetNewBook}>
+                  <Text style={styles.buttonText}>Hủy</Text>
                 </TouchableOpacity>
               </View>
-            </ScrollView>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-              <TouchableOpacity style={[styles.button, { backgroundColor: 'green' }]} onPress={isEditing ? handleEditBook : handleAddNew}>
-                <Text style={styles.buttonText}>{isEditing ? 'Sửa' : 'Thêm'}</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={[styles.button, { backgroundColor: 'red' }]} onPress={resetNewBook}>
-                <Text style={styles.buttonText}>Hủy</Text>
-              </TouchableOpacity>
             </View>
           </View>
-        </View>
-      </Modal>
+        </Modal>
 
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  container2: {
     flex: 1,
     padding: 10,
     backgroundColor: '#fff',
@@ -549,10 +556,10 @@ const styles = StyleSheet.create({
     shadowRadius: 15,
     elevation: 8,
   },
-  imagePreview:{
-    width: 100, 
+  imagePreview: {
+    width: 100,
     height: 100,
-    margin: 10 
+    margin: 10
   },
   categoryImage: {
     width: '100%',
